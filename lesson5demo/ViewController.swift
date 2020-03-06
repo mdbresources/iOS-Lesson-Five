c71e8f48073a4e2a6130b4106a80b3f3347ca898
Anmol Parande <parandea1.7@gmail.com>
2020-03-05 22:49:57 -0800

    Initial Commit

---
+++ lesson5demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson5demo
+//
+//  Created by Anmol Parande on 3/5/20.
+//  Copyright © 2020 Anmol Parande. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ lesson5demoTests/lesson5demoTests.swift
@@ -0,0 +1,34 @@
+//
+//  lesson5demoTests.swift
+//  lesson5demoTests
+//
+//  Created by Anmol Parande on 3/5/20.
+//  Copyright © 2020 Anmol Parande. All rights reserved.
+//
+
+import XCTest
+@testable import lesson5demo
+
+class lesson5demoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
